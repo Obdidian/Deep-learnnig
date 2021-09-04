a411a2f8f1c5219a0f93618c2ae0348ceea154c5
Home <21-10933@dshs.kr>
2021-09-04 23:09:50 +0900

    다차원 배열 곱셈

---
+++ 3.3.2.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+A=np.array([[1,2], [3,4]])
+print(A.shape)
+B=np.array([[5,6],[7,8]])
+print(B.shape)
+print(np.dot(A,B))
+
+print()
+
+A=np.array([[1,2,3], [4,5,6]])
+print(A.shape)
+B=np.array([[1,2],[3,4],[5,6]])
+print(B.shape)
+print(np.dot(A,B))
+
+print()
+
+A=np.array([[1,2], [3,4],[5,6]])
+print(A.shape)
+B=np.array([7,8])
+print(B.shape)
+print(np.dot(A,B))
