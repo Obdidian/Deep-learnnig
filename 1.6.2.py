b72e7d3ae8e618e87be9d5295461daf2a5747a04
Home <21-10933@dshs.kr>
2021-09-04 23:06:56 +0900

    numpy, matplotlib 학습

---
+++ 1.6.2.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+x=np.arange(0,6,0.1)
+y1=np.sin(x)
+y2=np.cos(x)
+
+plt.plot(x,y1,label="sin")
+plt.plot(x,y2,linestyle="--", label="cos")
+plt.xlabel("x")
+plt.xlabel("y")
+plt.title("sin & cos")
+plt.legend()
+plt.show()
